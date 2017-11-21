0e619fafd49d05c5a9eedfed0dcad1cb5a85221f
lin092392 <32370221+lin092392@users.noreply.github.com>
2017-11-20 21:49:37 -0600

    Update api.py

--- api.py
+++ api.py
@@ -7,3 +7,15 @@ import os
 #Api funtions
 def fact(n):
 	
+#I don't know what to put for the def
+#this is for prime number check
+
+if num > 1:
+   for i in range(2,num):
+       if (num % i) == 0:
+           print(num,"is not a prime number")
+           break
+   else:
+       print(num,"is a prime number")
+else:
+   print(num,"is not a prime number")
